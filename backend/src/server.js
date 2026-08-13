dbff1f84070b069fc6c53570b3af0a69d0fd1e7f
Bukunmi16 <bukunmiadigun14@gmail.com>
2026-08-13 17:35:54 +0100

    feat: add package.json for project management, update dependencies in frontend and backend, and configure axios base URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,38 @@ import {connectDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
-const app = express();
-const PORT = process.env.PORT || 3000;
+import path from 'path'
 
 // Load environment variables
 dotenv.config();
 
 // Database connection
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve()
+
 
 // Middleware
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+if (process.env.NODE_ENV !== "production") {      
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 // Route
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === "production") {      
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
